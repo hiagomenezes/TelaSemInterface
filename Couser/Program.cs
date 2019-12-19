9d0bf480560e25fab531840b5c88fe0c287a40f8
hiago roberto <hiago@itecnologia.com.br>
2019-12-19 17:04:27 -0300

    teste couser sem interface projeto udemy

---
+++ Couser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Couser.Entities;
+using Couser.Services;
+
+namespace Couser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per Hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Entre price per Day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            RentalServices rentalServices = new RentalServices(hour, day, new BrazilTaxService());
+
+            rentalServices.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+
+            Console.ReadKey();
+
+        }
+    }
+}
